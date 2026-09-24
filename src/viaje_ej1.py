ff0dc1c8d1c598b85b8ef4b8d47c9b9cdcea11b5
Javier <javrodbor@alum.us.es>
2026-09-24 16:08:38 +0200

    cambios clase

---
+++ src/viaje_ej1.py
@@ -0,0 +1,10 @@
+distancia_km = 384400  # distancia Tierra - Luna
+velocidad_kmh = 5000000
+tiempo_horas = distancia_km // velocidad_kmh
+tiempo_dias = tiempo_horas / 24
+tiempo_semanas = tiempo_dias // 7
+dias_sobrantes = tiempo_dias % 7
+print(f"Tardarías {tiempo_semanas} semanas y {dias_sobrantes} días en llegar.")
+
+#se muestran numeros decimales en la primera opción ya que la operación / nos devuelve una división EXACTA con sus numeros decimales,
+#mientras que la operación // nos devuelve solo la parte entera de esa división.
