0116c3d074a767080d406472a2562400d4ab6d20
Javier <javrodbor@alum.us.es>
2026-09-24 16:15:10 +0200

    cambios extra ej2

--- src/viaje_ej2.py
+++ src/viaje_ej2.py
@@ -2,6 +2,8 @@ distancia_km =int(input("Dime una distancia entre dos puntos cualquiera en kilom
 velocidad_kmh =int(input("Dime una velocidad en kilometros por hora: ")) 
 tiempo_horas = distancia_km / velocidad_kmh
 tiempo_dias = tiempo_horas / 24
-print(f"Tardarías {tiempo_dias} días en llegar.")
+tiempo_semanas = tiempo_dias // 7
+dias_restantes= tiempo_dias % 7
+print(f"Tardarías {tiempo_semanas} semanas y {dias_restantes} días en llegar.")
 
 #Ahora en este caso pedimos una distancia y una velocidad y calcularemos cuantos dias se tarda en llegar
