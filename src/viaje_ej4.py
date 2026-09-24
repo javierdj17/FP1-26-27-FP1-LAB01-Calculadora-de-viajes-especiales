54fd051484c6b88f1dadcfabef27a6eb346c2010
Javier <javrodbor@alum.us.es>
2026-09-24 16:26:08 +0200

    cambio ej 4 extra

--- src/viaje_ej4.py
+++ src/viaje_ej4.py
@@ -4,6 +4,8 @@ distancia_km = 225000000
 for i in range(10000,50000,10000):
     tiempo_horas = distancia_km / i
     tiempo_dias = tiempo_horas / 24 
-    print(f"velocidad: {i}km/h --> tiempos: {tiempo_dias} dias")
+    semanas = tiempo_dias // 7
+    dias_restantes = tiempo_dias % 7
+    print(f"velocidad: {i}km/h --> tiempos: {semanas} semanas y {dias_restantes} dias")
 
 #lo haremos con un for i in range y dentro de ese for ponemos el primer valor sera donde empieze, el segundo donde acaba, y el tercero (opcional), los saltos que va pegando, en este caso de 10.000 en 10.000
