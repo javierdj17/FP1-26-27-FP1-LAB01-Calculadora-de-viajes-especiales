1e992891f591ddd057334adfb4812dcbc56b9f49
Javier <javrodbor@alum.us.es>
2026-09-24 16:40:33 +0200

    cambio

--- src/viaje_extra2.py
+++ src/viaje_extra2.py
@@ -3,5 +3,5 @@ velocidad_kmh =float(input("Dime una velocidad en kilometros por hora: "))
 tiempo_horas = distancia_km / velocidad_kmh
 tiempo_dias = tiempo_horas // 24
 semanas = tiempo_dias // 7
-dias_restantes= tiempo_dias %7
+dias_restantes= tiempo_dias % 7
 print(f"Tardarías {semanas} semanas y {dias_restantes} días en llegar.")
